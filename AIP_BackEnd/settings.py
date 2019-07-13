17485b0f681b92b2d7529ab05e59a758150ece86
FKYnJYQ <loveress01@gmail.com>
2019-07-14 01:24:39 +0800

    remove web file, retrive from submodule

--- AIP_BackEnd/settings.py
+++ AIP_BackEnd/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'AIP_BackEnd.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'frontend', 'dist')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,6 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'frontend', 'dist',),
+]
 
 AUTH_USER_MODEL = 'aipsite.AipUser'
 
